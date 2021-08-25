4315b23ecffef3a4d031528c80d1220aaf8e8bb9
bhakti shah <kishandungarani2429@gmail.com>
2021-08-25 10:09:27 +0530

    set home screen in drawer

--- my_dasma/lib/ui/page_structure.dart
+++ my_dasma/lib/ui/page_structure.dart
@@ -1,16 +1,13 @@
-import 'dart:io';
-import 'dart:math' show pi;
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:my_dasma/extras/commonWidgets/CommonAppbar.dart';
 import 'package:my_dasma/extras/constants/AppColor.dart';
+import 'package:my_dasma/extras/constants/AppImages.dart';
 import 'package:my_dasma/extras/constants/StringConstant.dart';
 import 'package:provider/provider.dart';
 import 'home_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 class PageStructure extends StatelessWidget {
@@ -50,7 +47,47 @@ class PageStructure extends StatelessWidget {
           alignment: Alignment.center,
           color: Colors.white,
           child:
-          Text("${tr("current")}: ${HomeScreen.mainMenu[_currentPage].title}"),
+          ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index){
+
+
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 4.h, horizontal: 15.w),
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.r)
+                  ),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Ink.image(image: AssetImage(listViewImage3),
+                        height: 100.h,
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.7), BlendMode.dstATop),
+
+                        child: InkWell(
+                          onTap: (){
+
+                          },
+                        ),),
+                      Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.symmetric(horizontal: 16.0.w)
+                              ,child: Text(txtCatName, style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.bold),),
+                              alignment: Alignment.center,),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
